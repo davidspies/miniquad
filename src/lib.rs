1b9d364f2f98a37cb30063db5b3b798acc08547d
David Spies <dnspies@gmail.com>
2026-01-08 13:11:21 -0800

    Workspace-friendly changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 #![doc = include_str!("../README.md")]
-#![allow(
-    clippy::collapsible_if,
-    clippy::collapsible_else_if,
-    clippy::unused_unit,
-    clippy::identity_op,
-    clippy::missing_safety_doc
-)]
+#![allow(clippy::all, dead_code, static_mut_refs)]
 
 pub mod conf;
 mod event;
